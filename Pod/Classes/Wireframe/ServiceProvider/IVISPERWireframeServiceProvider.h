4f65ca2a47536add5c37f54cd77de22d867fdeab
Jan Bartel <barteljan@yahoo.de>
2015-08-02 19:34:21 +0200

    Removed UINavigationController dependency from wireframe

--- Pod/Classes/Wireframe/ServiceProvider/IVISPERWireframeServiceProvider.h
+++ Pod/Classes/Wireframe/ServiceProvider/IVISPERWireframeServiceProvider.h
@@ -16,28 +16,6 @@
 
 /**
  *
- * Provide routing presenters, which are responsible for creating the real controller transition, 
- * depending on some routing options (IVISPERRoutingOption)
- *
- */
-
-/**
- * add Routing presenter, responsible for routing controllers with specific RoutingOptions
- **/
--(void)addRoutingPresenter:(NSObject<IVISPERRoutingPresenter>*)presenter;
-
-/**
- * remove Routing presenter, responsible for routing controllers with specific RoutingOptions
- **/
--(NSArray*)removeRoutingPresentersResponsibleForRoutingOptions:(NSObject<IVISPERRoutingOption>*)routingOption;
-
-/**
- * get all registered routing presenters
- **/
--(NSArray*)routingPresenters;
-
-/**
- *
  * Provide some convienience RoutingOptions
  *
  */
