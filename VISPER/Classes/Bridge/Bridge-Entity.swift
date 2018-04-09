c611268c7c52196a68e71760c0cd5567a80aa41f
Jan Bartel <jan.bartel@atino.net>
2018-04-09 15:16:40 +0200

    fixed a typalias bug, renamed some deprecated functions

--- VISPER/Classes/Bridge/Bridge-Entity.swift
+++ VISPER/Classes/Bridge/Bridge-Entity.swift
@@ -13,6 +13,6 @@ public typealias CanBeIdentified = VISPER_Entity.CanBeIdentified
 public typealias ComposedEntityStore = VISPER_Entity.ComposedEntityStore
 public typealias Entity = VISPER_Entity.Entity
 public typealias EntityStore = VISPER_Entity.EntityStore
-public typealias FunctionalEntityStore<EntityType> = VISPER_Entity.FunctionalEntityStore<EntityType>
+public typealias FunctionalEntityStore<EntityType: CanBeIdentified> = VISPER_Entity.FunctionalEntityStore<EntityType>
 public typealias MemoryEntityStore = VISPER_Entity.MemoryEntityStore
 public typealias TypedEntityStore<Type> = VISPER_Entity.TypedEntityStore<Type>
